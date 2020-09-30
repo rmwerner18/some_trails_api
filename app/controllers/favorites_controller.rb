98cfc0891e9f53c535c09f79f8145f9fb92ab99c
humorous-storage-7521 <rmwerner@umich.edu>
2020-09-30 10:23:46 -0500

    no change

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,6 +10,11 @@ class FavoritesController < ApplicationController
         render json: favorite
     end
 
+    def destroy
+        favorite = Favorite.find(params[:id])
+        favorite.delete
+    end
+
     private
     def favorite_params
       params.require(:favorite).permit(:user_id, :trail_id)
--- app/serializers/trail_serializer.rb
+++ app/serializers/trail_serializer.rb
@@ -8,7 +8,7 @@ class TrailSerializer < ActiveModel::Serializer
 
   def to_serialized_json
     @trails.to_json(:include => {
-      :hikes => {:only => [:name, :user_id]}, :favorites => {:only => [:user_id]}}, :except => [:created_at, :updated_at]
+      :hikes => {:only => [:name, :user_id]}, :favorites => {:only => [:user_id, :id]}}, :except => [:created_at, :updated_at]
   )
   end
 
