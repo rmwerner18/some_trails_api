01d290840df799c0b84649a72d7873920a2b7a8e
Mimioj101 <mimioj@gmail.com>
2020-09-30 11:24:10 -0400

    changed seeds

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,6 +10,16 @@ class FavoritesController < ApplicationController
         render json: favorite
     end
 
+    def show
+        favorite = Favorite.find(params[:id])
+        render json: favorite
+    end
+
+    def destroy
+        favorite = Favorite.find(params[:id])
+        favorite.delete
+    end
+
     private
     def favorite_params
       params.require(:favorite).permit(:user_id, :trail_id)
