791bd31af8009c33f76833754aa0b0ff73d84a9a
Mimioj101 <mimioj@gmail.com>
2020-10-01 16:13:20 -0400

    setting up JWT

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
+
     def create
       @user = User.create(user_params)
       # byebug
       if @user.valid?
-        render json: UserSerializer.new(@user).to_serialized_json, status: :created
+        @token = encode_token(user_id: @user.id)
+        render json: UserSerializer.new(@user).to_serialized_json, jwt: @token, status: :created
       else
         render json: { error: 'failed to create user' }, status: :not_acceptable
       end
